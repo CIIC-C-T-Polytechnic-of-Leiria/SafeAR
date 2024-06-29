dafdbece9566f6acd655796e1bb426b8a8a2a86e
TiagoFRRibeiro <tiago.r.ribeiro@gmail.com>
2024-06-29 22:16:54 +0100

    setup update

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
+"""
+File: setup.py
+Author: Tiago F.R. Ribeiro
+Last Modified: 2024-06-29
+Description: 
+    This setup.py file is used to configure the metadata and dependencies for the SafeAR
+    - Obfuscation Service project. It uses setuptools to define the project's name, version, 
+    packages, dependencies, and entry points.
+"""
 from setuptools import setup, find_packages
 
+# Read the requirements from the requirements.txt file
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+# Setup configuration
 setup(
     name="SafeAR - Obfuscation Service",
     version="0.1.0",
@@ -10,10 +21,8 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "obfuscate = safeAR_obfuscation_aaS.cli:main",
+            "obfuscate = safeAR_obfuscation_aaS.cli:main"
         ],
     },
 )
-
-
 
--- src/dummy_client.py
+++ src/dummy_client.py
@@ -1,14 +1,25 @@
 """
 Dummy client to send an image to the Flask server for obfuscation.
+
+This script is part of a larger project and is used to send an image file to a Flask server for obfuscation.
+The image is read from the specified file path, encoded in base64, and then sent as a POST request to the server.
+The server's response is then printed to the console.
+
+Author: Tiago F. R. Ribeiro
+License: MIT
 """
 
 import argparse
 import base64
-
 import requests
 
-
 def send_image_to_server(image_path):
+    """
+    Function to send an image to the Flask server for obfuscation.
+
+    Args:
+    image_path (str): The path to the image file to send to the server.
+    """
     with open(image_path, "rb") as image_file:
         img_bytes = image_file.read()
         img_base64 = base64.b64encode(img_bytes).decode("utf-8")
@@ -21,7 +32,6 @@ def send_image_to_server(image_path):
     # Print the response status code and data
     print(f"Response status code: {response.status_code}")
     print(f"Obfuscated image data: {response.json()['image']}")
-
 
 if __name__ == "__main__":
     # Create an argument parser
